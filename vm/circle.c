a550cf46f231f99b7768757b4bf8d1faa1e83d30
Vladimir <qe.wtsn@gmail.com>
2021-01-07 20:56:14 +0300

    operations, new field in vm.h, test exc, begin of circle

--- vm/circle.c
+++ vm/circle.c
@@ -1,15 +1,55 @@
 #include "inc/vm.h"
 
+
+void	place_pl_and_kors(t_cw *cw)
+{
+	int position;
+	int player_id;
+
+	position = 0;
+	player_id = 0;
+
+	while (++player_id <= cw->num_of_champ)
+	{
+		ft_memcpy(&(cw->map[position]), vm->champs[id]->code, 
+		(size_t)vm->player_id[id]->code_size);
+		add_koretko(&vm->kors, create_koretko(cw->champs[player_id - 1], position));
+		position += MEM_SIZE / cw->num_of_champ;
+	}
+}
+
+void	make_op(t_cw *cw)
+{
+	t_koretko *cur;
+
+	cw->cycles++;
+	cur = cw->kors;
+	while (cur)
+	{
+
+	}
+}
+
 void circle(t_cw *cw)
 {
-	int i;
+	// int i;
 
-	i = -1;
-	cw->last_alive = cw->champs[0]->number;
-	cw->cycles_to_die = CYCLE_TO_DIE;
-	while (++i < cw->num_of_champ)
+	// i = -1;
+	// cw->last_alive = cw->champs[0]->number;
+	// cw->cycles_to_die = CYCLE_TO_DIE;
+	// while (++i < cw->num_of_champ)
+	// {
+	// 	ft_memcpy(&(vm->map))
+	// 	cw->kors[i] = (t_koretko*)ft_memalloc(sizeof(t_koretko));
+	// 	cw->kors[i]->id = i;
+	// }
+
+	while (cw->num_of_koretko)
 	{
-		cw->kors[i] = (t_koretko*)ft_memalloc(sizeof(t_koretko));
-		cw->kors[i]->id = i;
+		make_op(cw);
+		if (cw->cycles_to_die == cw->cycles)
+		{
+			check_cycles(cw);
+		}
 	}
 }
--- vm/test.c
+++ vm/test.c
@@ -8,25 +8,32 @@ void	op_add(t_koretko *kor, int *a, int *b, int *c)
 
 int main()
 {
-	t_cw *cw;
-	int	i = -1;
-	cw = (t_cw*)ft_memalloc(sizeof(t_cw));
-	cw->champs = (t_champ**)ft_memalloc(sizeof(t_champ*) * MAX_PLAYERS);
-	cw->champs[0] = (t_champ*)ft_memalloc(sizeof(t_champ));
-	cw->map = (unsigned char*)ft_memalloc(sizeof(char) * 4097);
-	cw->kors = (t_koretko**)ft_memalloc(sizeof(t_koretko*) * MAX_PLAYERS);
-	cw->rgtrs = (int*)ft_memalloc(sizeof(int) * REG_NUMBER);
-	while (++i < 4096)
-		cw->map[i] = 0;
-	cw->map[4096] = '\0';
+//	t_cw *cw;
+//	int	i = -1;
+//	cw = (t_cw*)ft_memalloc(sizeof(t_cw));
+//	cw->champs = (t_champ**)ft_memalloc(sizeof(t_champ*) * MAX_PLAYERS);
+//	cw->champs[0] = (t_champ*)ft_memalloc(sizeof(t_champ));
+//	cw->map = (unsigned char*)ft_memalloc(sizeof(char) * 4097);
+//	cw->kors = (t_koretko**)ft_memalloc(sizeof(t_koretko*) * MAX_PLAYERS);
+//	cw->rgtrs = (int*)ft_memalloc(sizeof(int) * REG_NUMBER);
+//	while (++i < 4096)
+//		cw->map[i] = 0;
+//	cw->map[4096] = '\0';
+//
+//	cw->rgtrs[0] = 1;
+//	cw->rgtrs[1] = -1;
+//	cw->rgtrs[2] = 10;
+//	cw->kors[0] = (t_koretko*)ft_memalloc(sizeof(t_koretko));
+//
+//	cw->kors[0]->carry = 5;
+//	op_add(cw->kors[0], &cw->rgtrs[0], &cw->rgtrs[1], &cw->rgtrs[2]);
+//	printf("%d carry %d", cw->rgtrs[2], cw->kors[0]->carry);
 
-	cw->rgtrs[0] = 1;
-	cw->rgtrs[1] = -1;
-	cw->rgtrs[2] = 10;
-	cw->kors[0] = (t_koretko*)ft_memalloc(sizeof(t_koretko));
-
-	cw->kors[0]->carry = 5;
-	op_add(cw->kors[0], &cw->rgtrs[0], &cw->rgtrs[1], &cw->rgtrs[2]);
-	printf("%d carry %d", cw->rgtrs[2], cw->kors[0]->carry);
+	int r = 0;
+	int a[6] = {3, 0, 3, 9, 2, 1};
+	r = 13;
+	r += a[a[0]];
+	r -= a[a[6-1]];
+	printf("%d", r);
 	return (0);
 }
